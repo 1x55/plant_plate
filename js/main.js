dc571bf7fb379b07d4b666be46c0d431010526a3
1x55 <k4i@pm.me>
2023-09-08 15:00:39 +0900

    added validation to userinput results

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 function getFetch() {
-    const choice = userInput
-    const url = ` https://world.openfoodfacts.org/api/v0/product/${choice}.json`
+    let inputVal = document.getElementById("barcode").value
+    const url = ` https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`
 
 fetch(url)
-    .then(res => res.join()) //parse response as JSON
-    .then(data => {
+    .then(res => res.json()) //parse response as JSON
+    .then(data => { 
         console.log(data)
+        if (data.status === 1 ) {
+            //call additional stuff
+        } else {
+            alert(`Product ${inputVal} was not found. Please try another`)
+        }
+
+
     })
     .catch(err => {
         console.log(`error ${err}`)
